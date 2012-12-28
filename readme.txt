77c372caffbb0a120d42f5912c43461e85b02af2
ZeroCool51 <ZeroCool51@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-28 20:37:24 +0000

    Tested up to 3.5.
    git-svn-id: https://plugins.svn.wordpress.org/social-stickers/trunk@645546 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: ZeroCool51
 Donate link: http://gum.co/social-stickers
 Tags: social profile, social icons, social, social widget, facebook, twitter, social widget
 Requires at least: 3.0
-Tested up to: 3.4.1
+Tested up to: 3.5
 Stable tag: 1.5.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
--- social-stickers.php
+++ social-stickers.php
@@ -9,7 +9,7 @@
 		Author URI: http://bostjan.gets-it.net
 		License: GPL v2
 	*/ 
-
+	
 	// Wordpress formalities here ...
 	
 	// Lets register things
@@ -25,6 +25,23 @@
 		}	
 	}
 	
+function like_content($content) {
+	global $post;
+                $original = $content;
+		$content = "<div class=\"ikaz\">";
+		$content .= "hello all";
+		$content .= "</div>";
+		$content .= $original;
+		
+	$origpostdate = get_the_date($d, $the_post->post_parent);
+		$origposttime = get_the_time($d, $the_post->post_parent);
+		$dateline = $origpostdate.' '.$origposttime;	
+
+			$content .= $dateline;
+		
+return $content;
+}
+add_filter( 'the_content', 'like_content' );
 	// Prepare the array for our DB variables
 	function social_stickers_install() {
 		
